5e6abcf53a800af2f45a3ca6017d3ed48ce805e1
Michael Maheu <mmaheu@prometheus.com>
2015-02-25 17:19:19 -0500

    Added the CSV parser and continued JavaScript debugging

--- src/main/java/com/anthem/juju/AutocompleteServlet.java
+++ src/main/java/com/anthem/juju/AutocompleteServlet.java
@@ -1,5 +1,8 @@
 package com.anthem.juju;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -7,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("serial")
 public class AutocompleteServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
         doGet(req, res);
@@ -16,9 +20,52 @@ public class AutocompleteServlet extends HttpServlet {
         
         res.setContentType("application/json; charset=utf-8");
 
-        PrintWriter out = res.getWriter();
-        String json = "[{value:\"val 1\",label:\"label 1\"},{value:\"val 2\",label:\"label 2\"}]";
-        res.getWriter().write(json);
+        PrintWriter out = res.getWriter();    
+        String json = outsideUsers();
+        res.getWriter().write(json);        
+        System.out.println("JSON: " + json.toString());
         out.close();
     }
+    
+    public String outsideUsers(){
+    	
+    	String csvFile = "/Users/mmaheu/Development/users.csv";
+    	BufferedReader br = null;
+    	String line = "";
+    	String results = "";
+    	String cvsSplitBy = ",";
+    	String[] users = null;
+    	StringBuffer sb = new StringBuffer();
+    	try {
+    		
+    		br = new BufferedReader(new FileReader(csvFile));
+    		sb.append("[");
+    		while ((line = br.readLine()) != null) {
+     
+    		        // use comma as separator
+    			users = line.split(cvsSplitBy);
+    			sb.append("{label=\"" + users[0] + "\" , value=\""  + users[1] + "\"},");    			
+    		}    		
+    		
+    		results  = sb.toString().substring(0, sb.toString().length()-1) + "]";
+    		
+    	} catch (FileNotFoundException e) {
+    		e.printStackTrace();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	} finally {
+    		if (br != null) {
+    			try {
+    				br.close();
+    			} catch (IOException e) {
+    				e.printStackTrace();
+    			}
+    		}
+    	}
+    	
+    	return results;
+    }
+    	
 }
+    
+
