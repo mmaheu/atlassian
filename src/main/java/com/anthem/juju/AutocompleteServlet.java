1e0093242d4564e5257b1cdce6d548ba515eeed5
Michael Maheu <mmaheu@prometheus.com>
2015-02-26 15:14:42 -0500

    javascript debugging

--- src/main/java/com/anthem/juju/AutocompleteServlet.java
+++ src/main/java/com/anthem/juju/AutocompleteServlet.java
@@ -12,60 +12,60 @@ import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class AutocompleteServlet extends HttpServlet {
+
+
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
         doGet(req, res);
     }
-    
+
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        
+
         res.setContentType("application/json; charset=utf-8");
 
-        PrintWriter out = res.getWriter();    
+        PrintWriter out = res.getWriter();
         String json = outsideUsers();
-        res.getWriter().write(json);        
+        res.getWriter().write(json);
         System.out.println("JSON: " + json.toString());
         out.close();
     }
-    
-    public String outsideUsers(){
-    	
-    	String csvFile = "/Users/mmaheu/Development/users.csv";
-    	BufferedReader br = null;
-    	String line = "";
-    	String results = "";
-    	String cvsSplitBy = ",";
-    	String[] users = null;
-    	StringBuffer sb = new StringBuffer();
-    	try {
-    		
-    		br = new BufferedReader(new FileReader(csvFile));
-    		sb.append("[");
-    		while ((line = br.readLine()) != null) {
-     
-    		        // use comma as separator
-    			users = line.split(cvsSplitBy);
-    			sb.append("{label=\"" + users[0] + "\" , value=\""  + users[1] + "\"},");    			
-    		}    		
-    		
-    		results  = sb.toString().substring(0, sb.toString().length()-1) + "]";
-    		
-    	} catch (FileNotFoundException e) {
-    		e.printStackTrace();
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    	} finally {
-    		if (br != null) {
-    			try {
-    				br.close();
-    			} catch (IOException e) {
-    				e.printStackTrace();
-    			}
-    		}
-    	}
-    	
-    	return results;
-    }
-    	
-}
-    
 
+    public String outsideUsers(){
+
+      String csvFile = "/Users/mmaheu/Development/users.csv";
+      BufferedReader br = null;
+      String line = "";
+      String results = "";
+      String cvsSplitBy = ",";
+      String[] users = null;
+      StringBuffer sb = new StringBuffer();
+      try {
+
+        br = new BufferedReader(new FileReader(csvFile));
+        sb.append("[");
+        while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+          users = line.split(cvsSplitBy);
+          sb.append("{label=\"" + users[0] + "\" , value=\""  + users[1] + "\"},");
+        }
+
+        results  = sb.toString().substring(0, sb.toString().length()-1) + "]";
+
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+      } catch (IOException e) {
+        e.printStackTrace();
+      } finally {
+        if (br != null) {
+          try {
+            br.close();
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+
+      return results;
+    }
+
+}
--- src/main/resources/js/datauserselect.js
+++ src/main/resources/js/datauserselect.js
@@ -1,14 +1,10 @@
 JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function (e, context) {juju_init(context)});
 
 function juju_init(context) {
-    AJS.$("#juju-user-search").autocomplete({
+    AJS.$("#juju-user-search").autocomplete(
+      {
         source: "http://localhost:2990/jira/plugins/servlet/juju/user-search",
-        minLength: 2       
+        minLength: 2
         }
     );
-    
-    var strJSON = "[{label='Mike Maheu' , value='mmaheu'},{label='Scott Wilson' , value='swilson'}]"
-    var objJSON = eval("(function(){return " + strJSON + ";})()");
-    alert(objJSON.result);
-    alert(objJSON.count); 
 }
